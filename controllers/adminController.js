115e86b406468828c4ca30a219eded1009184d4d
Andrew-21-1 <andrew.ashraf@live.com>
2019-05-18 01:26:51 +0200

    make it better

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 var Admin = require('../models/admin');
-const validator = require('../Validations/adminValidations')
+const validator = require('')
 
 exports.createAdmin = async function (req,res){
     try {
