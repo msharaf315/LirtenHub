18b273f7481a07cc22379790ca963d5329ff4f24
Andrew-21-1 <andrew.ashraf@live.com>
2019-05-18 05:26:13 +0200

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const passport = require('passport')
 const path = require('path');
+const favicon = require('express-favicon');
 
 
 
@@ -54,25 +55,15 @@ app.use('/api/vacancy', vacancy)
 
 const port = process.env.PORT || 5000
 
-//Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-//production mode
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
-
-
-
-// Handling 404
-app.use((req, res) => {
-    res.status(404).send({err: 'We can not find what you are looking for'});
- })
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
